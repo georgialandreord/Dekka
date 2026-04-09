57bd507cf9bf4c7c38ff340c8be46a041a80e40d
sunil-dsb <sunilnegidsb@gmail.com>
2026-04-09 19:07:43 +0530

    test client issue

--- src/server/better-auth/client.ts
+++ src/server/better-auth/client.ts
@@ -1,7 +1,7 @@
 import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 import type { auth } from "./config";
-import { polarClient } from "@polar-sh/better-auth";
+import { polarClient } from "@polar-sh/better-auth/client";
 
 export const authClient = createAuthClient({
   plugins: [inferAdditionalFields<typeof auth>(), polarClient()],
